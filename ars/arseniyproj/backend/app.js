83abfc6a51779f2aaef77876d7c00ac767bcdfd9
spo <spo@center.mddm>
2020-03-14 18:02:13 +0200

    mongodb

--- ars/arseniyproj/backend/app.js
+++ ars/arseniyproj/backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
 
 const login = require("./modules/login/routes");
 
@@ -10,6 +11,13 @@ app.use(bodyParser.json());
 
 app.use("/login", login);
 
-var server = app.listen(3000, function() {
+mongoose
+  .connect(
+    "mongodb+srv://Griz:fuck1ng_asshol6@firstproject-dljrj.gcp.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("DB CONNECTED"));
+
+const server = app.listen(3000, function() {
   console.log("Node server is running on port 3000");
 });
